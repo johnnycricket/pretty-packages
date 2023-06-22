92756572044bc1a5ca0f78b5790bc6bfa7021825
John Vorwald <cricketjohnny@gmail.com>
2023-06-22 13:23:13 -0500

    adding more tests.

--- src/options/getUrl.test.ts
+++ src/options/getUrl.test.ts
@@ -1,1 +1,24 @@
-export {}
+import { Options } from "./Options";
+import { getUrl } from "./getUrl";
+
+describe('test getUrl', () => {
+    const mockOptions: Options = {
+        includeUrl: true
+    }
+
+    beforeEach(() => {
+        mockOptions.includeUrl = true;
+    })
+
+    it('should call getUrl with true option and dep name and return a url string', () => {
+        const actual = getUrl(mockOptions, 'some-package');
+        expect(actual).toEqual('https://www.npmjs.com/package/some-package');
+    });
+
+    it('should call getUrl with false option and return undefined', () => {
+        mockOptions.includeUrl = false;
+
+        const actual = getUrl(mockOptions, 'some-package')
+        expect(actual).toBeFalsy();
+    })
+})
--- src/options/getVersion.test.ts
+++ src/options/getVersion.test.ts
@@ -1,1 +1,27 @@
-export {}
+import { Options } from "./Options";
+import { getVersion } from "./getVersion";
+
+describe('test getVersion', () => {
+    const mockOptions: Options = {
+        includeVersion: true
+    }
+
+    beforeEach(() => {
+        mockOptions.includeVersion = true;
+    })
+
+    test('getVersion should return string of package\'s version when option exists and dependency is provided', () => {
+        const actual = getVersion(mockOptions, '^1.2.3');
+        expect(actual).toBe('^1.2.3');
+    })
+    test('getVersion should return undefined if option is false', () => {
+        mockOptions.includeVersion = false;
+        const actual = getVersion(mockOptions, '^1.2.3');
+        expect(actual).toBeFalsy();
+    })
+    test('getVersion should return undefined if version option not included', () => {
+        delete mockOptions.includeVersion;
+        const actual = getVersion(mockOptions, '^1.2.3');
+        expect(actual).toBeFalsy();
+    })
+})
